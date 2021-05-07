fa566aba7f02a23148b302790b3c41e1207d97a0
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-05-07 15:12:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-    header("HTTP/1.1 403 Forbidden");
-    header("Status: 403 Forbidden");
-    echo "Default page.";
+    //header("HTTP/1.1 403 Forbidden");
+    //header("Status: 403 Forbidden");
+    //echo "Default page.";
+echo json_encode($_SERVER, JSON_PRETTY_PRINT);
